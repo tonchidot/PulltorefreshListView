ac6dcfb6765f506477e5737f5d1d9012c8616012
Makoto Hamanaka <m-hamanaka@tonchidot.com>
2013-04-22 03:22:14 +0900

    Revert "removed redundant setting of header background"
    This reverts commit 56ecfb733f668028c49b537c42554f776a9320b4.

--- library/src/yaochangwei/pulltorefreshlistview/widget/extend/PullToRefreshListView.java
+++ library/src/yaochangwei/pulltorefreshlistview/widget/extend/PullToRefreshListView.java
@@ -26,6 +26,7 @@ public class PullToRefreshListView extends RefreshableListView {
 		super(context, attrs);
 
 		setContentView(R.layout.pull_to_refresh);
+		mListHeaderView.setBackgroundColor(Color.parseColor("#e0e0e0"));
 		setOnHeaderViewChangedListener(new OnHeaderViewChangedListener() {
 
 			@Override
